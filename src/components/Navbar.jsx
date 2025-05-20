2b4ac17869fbc3743aa66b239b5956a846c056cb
“kamanbo23” <lkamanboina@gmail.com;git config --global user.email lkamanboina@gmail.com;/git checkout -b LokeshKamanboina>
2025-05-20 10:41:42 -0700

    changed navbar to expandable on mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { 
   AppBar, 
   Box, 
   Toolbar, 
   Button, 
-  useTheme 
+  useTheme,
+  IconButton,
+  Menu,
+  MenuItem,
+  useMediaQuery
 } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 import { Link } from 'react-router-dom';
 
 // Basic navbar with links to main pages
-// need to add mobile menu at some point if its unorganized
+// Uses expandable menu on mobile devices
 function Navbar() {
   // get theme for consistent colors
   const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const [anchorEl, setAnchorEl] = useState(null);
+  
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
   
   // tried to use styled components here but ran into issues
   // sticking with sx prop for now
@@ -35,16 +50,35 @@ function Navbar() {
             </Link>
           </Box>
           
-          <Button color="inherit" component={Link} to="/">Home</Button>
-          <Button color="inherit" component={Link} to="/big-board">Big Board</Button>
-          <Button 
-            color="inherit" 
-            component={Link} 
-            to="/data-viz"
-          >
-            Data Viz
-          </Button>
-          <Button color="inherit" component={Link} to="/projection">AI Projections</Button>
+          {isMobile ? (
+            <Box>
+              <IconButton
+                color="inherit"
+                aria-label="menu"
+                onClick={handleMenuOpen}
+                edge="start"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={handleMenuClose}
+              >
+                <MenuItem onClick={handleMenuClose} component={Link} to="/">Home</MenuItem>
+                <MenuItem onClick={handleMenuClose} component={Link} to="/big-board">Big Board</MenuItem>
+                <MenuItem onClick={handleMenuClose} component={Link} to="/data-viz">Data Viz</MenuItem>
+                <MenuItem onClick={handleMenuClose} component={Link} to="/projection">AI Projections</MenuItem>
+              </Menu>
+            </Box>
+          ) : (
+            <>
+              <Button color="inherit" component={Link} to="/">Home</Button>
+              <Button color="inherit" component={Link} to="/big-board">Big Board</Button>
+              <Button color="inherit" component={Link} to="/data-viz">Data Viz</Button>
+              <Button color="inherit" component={Link} to="/projection">AI Projections</Button>
+            </>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
